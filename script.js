998fa3e807a384384f146c53038fa8376d872e05
Greville <greville1664@gmail.com>
2025-12-08 11:08:43 -0300

    Tabela colspan e rowspan

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 let id = 0
+let numberRow = 1
+
+
 function btn() {
     const input = document.getElementById('name')    
     id++ 
@@ -12,25 +15,35 @@ function btn() {
         document.getElementById('footer').style.display = ''
         return
     }
+    
     const tr = document.createElement('tr')
     tr.innerHTML = `
-        <td >${id}</td>
-        <td>${input.value}</td>
-        <td colspan="2">${randomNumber}</td>
-        <td><button type="button" class="btn btn-danger btn-sm ">Remover</button></td>
+        <td class="border">${id}</td>
+        <td class="border">${input.value}</td>
+        <td class="border">${randomNumber}</td>
+        <td><button type="button" class="btn btn-danger btn-sm">Remover</button></td>
     `
+    numberRow++
     const tbody = document.querySelector('tbody')
     tbody.appendChild(tr)
     input.value = ''
     document.getElementById('footer').style.display = ''
     setTimeout(() => {
             document.getElementById('footer').style.display = 'none'
-        }, 2000)
+        }, 3000)
 
     const btn = tr.querySelector('button')
     btn.addEventListener('click', function() {
         tr.remove()
+        numberRow--
+        if (numberRow <= 6) {
+            document.getElementById('mainTable').style.display = 'none'
+        }
     })
+
+    if (numberRow >= 6) {
+        document.getElementById('mainTable').style.display = ''
+    }
 }
 
 document.addEventListener('keydown', function(event) {
